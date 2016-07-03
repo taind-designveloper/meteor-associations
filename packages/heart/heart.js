2136db4c315d7327e165f57a730813f7fa55489a
duy tai nguyen <taind@designveloper.com>
2016-07-03 23:19:28 +0700

    demo share

--- packages/heart/heart.js
+++ packages/heart/heart.js
@@ -8,7 +8,7 @@ users.has(posts, new readPost());
 
 posts.allow({
   insert(userId, post) {
-    let insert = new ownedPost().associate().insert;
+    let insert = new ownedPost().insert;
     return insert.apply({
       _id: userId,
     }, [post]);
@@ -17,6 +17,19 @@ posts.allow({
     if(!userId) return false;
     let user = users.findOne(userId);
     return user.belongsTo('ownedPost', post._id);
+  },
+  update(userId, post, fields, modifier) {
+    if(!userId) return false;
+    let user = users.findOne(userId);
+    // owner of this post
+    if(user.belongsTo('ownedPost', post._id)) {
+      return Match.test(modifier, {
+        $push: {
+          'permission.reads': String,
+        }
+      })
+    }
+    return false;
   }
 });
 
