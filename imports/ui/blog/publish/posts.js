edb573da5fa48d81f6d5ca53ea3c9c784256434e
duy tai nguyen <taind@designveloper.com>
2016-07-04 19:53:23 +0700

    improve post

--- imports/ui/blog/publish/posts.js
+++ imports/ui/blog/publish/posts.js
@@ -1,4 +1,19 @@
-Meteor.publish('posts.all', function() {
-  if(!this.userId) return this.ready();
-  return users.findOne(this.userId).readPost.find({});
-});
+Meteor.publishComposite('posts.all', {
+  find() {
+    if(!this.userId) return this.ready();
+    return users.findOne(this.userId).readPost.find({});
+  },
+  children: [
+    {
+      find(post) {
+        let permission = post.permission;
+        let userId = [];
+        for(let prop in permission) {
+          userId = userId.concat(permission[prop]);
+        }
+        userId = _.uniq(userId);
+        return users.find({_id: {$in: userId}});
+      }
+    }
+  ]
+})
